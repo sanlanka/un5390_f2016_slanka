19fddb25042134caed211aa2d4325271bc31d317
Gowtham <g@mtu.edu>
2016-09-29 08:06:56 -0400

    Updated course material for week #05

---
+++ CourseWork/Week_05/AdditionalMaterial/AutomatedCompilation/MultipleSourceFiles/pi.c
@@ -0,0 +1,76 @@
+// pi.c
+//
+// C program to compute the value of 'pi' for a given number of terms using 
+// the Newton and Madhava approximations. Compilation/Execution takes less 
+// than one second on most modern hardware running linux OS with GCC 4.8.5.
+//
+// Compilation (without Makefile; with one command):
+// gcc -g -Wall pi.c pi_1.c pi_2.c -lm -o pi.x
+//
+// Compilation (without Makefile; with multiple commands):
+// gcc -g -Wall -c pi.c
+// gcc -g -Wall -c pi_1.c
+// gcc -g -Wall -c pi_2.c
+// gcc pi.o pi_1.o pi_2.o -lm -o pi.x
+//
+// Compilation (with Makefile; one of the following commands):
+// cp Makefile_[Simple|Concise|Elegant] Makefile
+// make 
+// make all
+// make pi.x
+//
+// Execution:
+// ./pi.x
+
+// Headers
+#include <stdio.h>  // Core input/output operations
+#include <stdlib.h> // Conversions, random numbers, memory allocation, etc.
+#include <math.h>   // Common mathematical functions
+#include <time.h>   // Converting between various date/time formats
+#include <unistd.h> // Hostname, etc.
+
+// Function declaration
+long double pi_newton(int n);
+long double pi_madhava(int n);
+long double pi_error(long double PI, long double PI_C);
+
+// main()
+int main() {
+
+  // Variable declaration
+  long double PI = 3.141592653589793; // Known value of PI
+  int n = 75;                         // Number of terms
+
+  // Print the problem statement
+  printf("\n");
+  printf("  C program to evaluate PI using the Newton and Madhava approximations\n");
+
+  // Evaluate PI_1 and PI_2
+  long double PI_1 = pi_newton(n);
+  long double PI_2 = pi_madhava(n);
+
+  // Calculate the error in PI_1 and PI_2
+  long double ERROR_PI_1 = pi_error(PI, PI_1);
+  long double ERROR_PI_2 = pi_error(PI, PI_2);
+
+  // Print the summary
+  printf("\n");
+  printf("                n : %d\n", n);
+  printf("               PI : %16.15Lf\n", PI);
+  printf("     PI_1 (Error) : %16.15Lf (%16.15Lf) \n", PI_1, ERROR_PI_1);
+  printf("     PI_2 (Error) : %16.15Lf (%16.15Lf) \n\n", PI_2, ERROR_PI_2);
+
+  // Indicate termination
+  return 0;
+}
+
+// pi_error()
+// Evaluates the error in computed value of PI
+long double pi_error(long double PI, long double PI_C) {
+
+  // Calculate the error in PI_C
+  long double PI_ERROR = fabs((PI - PI_C)/PI) * 100;
+
+  // Return error to parent module
+  return PI_ERROR;
+}
---
+++ CourseWork/Week_05/AdditionalMaterial/c2gnuplot_explicit.c
@@ -0,0 +1,105 @@
+// c2gnuplot_explicit.c
+//
+// A program to demonstrate the integration of Gnuplot as a visualization tool
+// with C. The program generates a pair of given number of pseudo random 
+// numbers between specified lower and upper limits, and uses them as (x, y)
+// coordinates to plot outside of the core computation loop. Compilation takes 
+// less than one second, and execution can take several seconds (depending on 
+// the values of N and interval variables) on most modern hardware running 
+// linux OS with GCC 4.8.5
+//
+// Compilation and execution:
+// gcc -g -Wall c2gnuplot_explicit.c -lm -o c2gnuplot_explicit.x
+// ./c2gnuplot_explicit.x
+
+// Headers
+#include <stdio.h>    // Core input/output operations
+#include <stdlib.h>   // Conversions, random numbers, memory allocation, etc.
+#include <string.h>   // String operations
+#include <unistd.h>   // Access to the POSIX operating system API
+#include <math.h>     // Common mathematical functions
+#include <time.h>     // Converting between various date/time formats
+#include <sys/time.h> // Additional time operations
+
+// random_number_generator()
+// Given two numbers, -R and R, this function generates a random number 
+// between them.
+double random_number_generator(double a, double b) {
+
+  // Variable declaration
+  double prn; // Pseudo-random number
+
+  // Generate a pseudo-random number between a and b
+  prn = ((b - a) * ((double) rand()/(double) RAND_MAX)) + a;
+
+  // Return the value of pseudo-random number
+  return prn;
+}
+
+// main()
+int main() {
+
+  // Variable declaration
+  int i;                // Loop index
+  int N = 256;          // Number of random numbers
+  int seed = 1000000;   // Seed for random number generator
+  int interval = 50000; // Sleep interval (micro seconds)
+  double llimit = -1.0; // Lower limit for random numbers
+  double ulimit = 1.0;  // Upper limit for random numbers
+  double x, y;          // Random numbers
+  FILE *file;           // File pointer for data file
+  FILE *pipe;           // File pointer for Gnuplot
+
+  // Seed the random number generator
+  srand(seed);  
+
+  // Open a connection to file
+  file = fopen("c2gnuplot_explicit.dat", "w");
+
+  // Open a persistent connection to Gnuplot
+  pipe = popen("gnuplot -persist", "w");
+
+  // Gnuplot settings (basic)
+  fprintf(pipe, "set title 'Distribution of %d random numbers' \n", N);
+  fprintf(pipe, "set xlabel 'x' \n");
+  fprintf(pipe, "set ylabel 'y' \n");
+  fprintf(pipe, "set grid \n");
+  fprintf(pipe, "set pointsize 1 \n");
+  fprintf(pipe, "set xrange[-1.1:1.1] \n");
+  fprintf(pipe, "set yrange[-1.1:1.1] \n");
+
+  // Core computation loop
+  for (i = 1; i <= N; i++) {
+
+    // Random number generation
+    x = random_number_generator(llimit, ulimit);
+    y = random_number_generator(llimit, ulimit);
+
+    // Write the data to file
+    fprintf(file, "%-18.14lf %-18.14lf \n", x, y);
+  }
+
+  // Close the file
+  fclose(file);
+
+  // Visualization loop
+  for (i = 1; i <= N; i++) {
+
+    // It may not be necessary to plot every data point
+    fprintf(pipe, "plot 'c2gnuplot_explicit.dat' every ::1::%d using ($1):($2) with points pt 6 notitle \n", i);
+
+    // Flush the pipe to update the plot
+    fflush(pipe);
+
+    // Pause the process for 'interval' micro seconds.
+    // This will be unnecessary if the core computation takes a significant
+    // amount of time.
+    usleep(interval);
+  }
+
+  // Close the pipe
+  fclose(pipe);
+
+  // Indicate termination
+  return 0;
+}
