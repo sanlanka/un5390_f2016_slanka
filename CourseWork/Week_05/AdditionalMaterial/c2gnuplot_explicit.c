18c49bfb5aa1ba8369bc63381e2495ab445f6c85
Gowtham <g@mtu.edu>
2016-09-29 11:05:40 -0400

    Updated course material and grade through week #05

--- CourseWork/Week_05/AdditionalMaterial/c2gnuplot_explicit.c
+++ CourseWork/Week_05/AdditionalMaterial/c2gnuplot_explicit.c
@@ -6,7 +6,10 @@
 // coordinates to plot outside of the core computation loop. Compilation takes 
 // less than one second, and execution can take several seconds (depending on 
 // the values of N and interval variables) on most modern hardware running 
-// linux OS with GCC 4.8.5
+// Linux OS with GCC 4.8.5.
+//
+// Plots the data after the data has been completely generated and reading it
+// from a file on the disk.
 //
 // Compilation and execution:
 // gcc -g -Wall c2gnuplot_explicit.c -lm -o c2gnuplot_explicit.x
@@ -60,7 +63,7 @@ int main() {
   pipe = popen("gnuplot -persist", "w");
 
   // Gnuplot settings (basic)
-  fprintf(pipe, "set title 'Distribution of %d random numbers' \n", N);
+  fprintf(pipe, "set title 'Distribution of %d random numbers (explicit plot)' \n", N);
   fprintf(pipe, "set xlabel 'x' \n");
   fprintf(pipe, "set ylabel 'y' \n");
   fprintf(pipe, "set grid \n");
