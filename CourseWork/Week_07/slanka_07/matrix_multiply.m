19966345944201a275d2bc83ec7c8baf40a92e0c
Sandeep Lanka <slanka@mtu.edu>
2016-11-06 01:49:22 -0400

    Submitting the verification part of matrix_multiply.m program

--- CourseWork/Week_07/slanka_07/matrix_multiply.m
+++ CourseWork/Week_07/slanka_07/matrix_multiply.m
@@ -10,20 +10,21 @@
 %All Matrices are represented as column vectors meaning that if we want to ..
 %enter values in a matrix they are going to be arranged in straight columns
 %[1,2,3;1,2,3;1,2,3] will be represented as[1,2,3,1,2,3,1,2,3] 
-
+clc;
+clear;
 
 N = [2,3,4]; %RxV matrix
-Nx1matrix = N;
-NxNmatrix = [2,3,4,2,3,4,2,3,4];%CxR
+A = [2,3,4,2,3,4,2,3,4];%CxR
+B = N;
 %NxNmatrix = [Nx1matrix;Nx1matrix;Nx1matrix];
-[~,c] = size(Nx1matrix);
+[~,c] = size(B);
 co = 0;%Count to Jump Columns(Represneted as straight rows)
 ii = 1;
-mulMatrix = zeros(1,3);
+C = zeros(1,c);
 for i = 1:c
     for j = 1:c
-    mulMatrix(ii) = mulMatrix(ii)...
-                    +(NxNmatrix((co*c)+j)*Nx1matrix(j));
+    C(ii) = C(ii)...
+                    +(A((co*c)+j)*B(j));
     end
         
         co = co + 1;
@@ -31,4 +32,4 @@ for i = 1:c
 end
 %Displays Multiplied matrix on the Command Prompt
 disp('Result:')
-disp(mulMatrix');
+disp(C');
