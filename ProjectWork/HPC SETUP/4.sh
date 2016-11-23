c8febb291c70f61ddce6e7b4c4edff5044797cc6
Gowtham <g@mtu.edu>
2016-11-23 07:58:50 -0500

    Deleted HPC SETUP folder

--- ProjectWork/HPC SETUP/4.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Author: Prithvi Kambhampati
-echo "disable monitor" >> /etc/ntp.conf
-/etc/init.d/ntpd restart
--- ProjectWork/HPC SETUP/6.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Author: Prithvi Kambhampati
-yum install package.i686 package.x86_64
